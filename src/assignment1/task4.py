6d1e4dd4306e3a5e96579d70d1fe9104a0b447a1
Erik Junsved <dev@erix.dev>
2024-11-06 23:14:42 +0100

    final changes to task4 on assignment 1

--- src/assignment1/task4.py
+++ src/assignment1/task4.py
@@ -3,10 +3,8 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import sounddevice as sd
 
-# The unit of time used is seconds
-
 start = 0.0
-stop = 5.0
+stop = 0.02
 
 increment = 0.00001
 
@@ -16,19 +14,33 @@ x1 = np.sin(200 * np.pi * t)
 x2 = np.sin(2000 * np.pi * t)
 x3 = x1 + x2
 
-fig, ax = plt.subplots()
-ax.plot(t, x1, label="x_1(t)")
-ax.plot(t, x2, label="x_2(t)")
-ax.plot(t, x3, label="x_3(t)")
+alpha = 1000 * np.pi
 
-ax.set_xlim(0, 0.01)
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Amplitude")
 
-ax.legend()
+def unit_step(t):
+    return np.where(t >= 0, 1, 0)
+
+u_t = unit_step(t)
+h_t = np.pow(alpha, 2) * t * np.pow(np.e, -alpha * t) * u_t
+
+y1 = increment * signal.convolve(x1, h_t, method="direct")
+y2 = increment * signal.convolve(x2, h_t, method="direct")
+y3 = increment * signal.convolve(x3, h_t, method="direct")
+
+y1 = y1[0:int(0.02 / increment) + 1:1]
+y1 = y1[0:t.shape[0]]
+
+y2 = y2[0:int(0.02 / increment) + 1:1]
+y2 = y2[0:t.shape[0]]
+
+y3 = y3[0:int(0.02 / increment) + 1:1]
+y3 = y3[0:t.shape[0]]
+
+y1_y2 = y1 + y2
+
+fig, bx = plt.subplots()
+#bx.plot(t, y3, label="y3(t)")
+bx.plot(t, y1_y2, label="y1(t)+y2(t)")
+plt.legend()
 plt.grid()
 plt.show()
-
-sd.play(x1, 1/increment, blocking=True)
-sd.play(x2, 1/increment, blocking=True)
-sd.play(x3, 1/increment, blocking=True)
